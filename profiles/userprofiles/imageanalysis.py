1d73762e7c4d39f05336f6e4aed3155136f4c984
volaya <volayaf@gmail.com>
2016-05-04 16:50:45 +0200

    bug fixes

--- profiles/userprofiles/imageanalysis.py
+++ profiles/userprofiles/imageanalysis.py
@@ -3,20 +3,27 @@
 # (c) 2016 Boundless, http://boundlessgeo.com
 # This code is licensed under the GPL 2.0 license.
 #
-from processing.gui.utils import *
+from processing.gui.menus import *
 from processing.core.Processing import Processing
-from profilesplugin.utils import addActionAt
+from profiles.utils import addActionAt
 from qgis.utils import *
 from catalogpl_plugin import CatalogPLPlugin
 from PyQt4.Qt import QAction
+from PyQt4.QtGui import QMessageBox
 
 def addProviderAsMenus(providerName, menuName):
     provider = Processing.getProviderFromName(providerName)
-    for alg in provider.algs:
-        addAlgorithmEntry(alg.commandLineName(), menuName, alg.group)
+    if provider.algs:
+        for alg in provider.algs:
+            addAlgorithmEntry(alg, menuName, alg.group)
+    else:
+        QMessageBox.warning(iface.mainWindow(), "Plugin installation",
+                        "OTB provider is not configured in Processing.\n"
+                        "Image analysis tools could not be configured.",
+                        QMessageBox.Ok, QMessageBox.Ok) 	
 
 def apply():
-    addProviderAsMenus("otb", "Image analysis")
+    addProviderAsMenus("saga", "Image analysis")
     plugin = plugins["catalogpl_plugin"]
     action = QAction(plugin.icon, "Planet Labs layer", iface.mainWindow())
     action.triggered.connect(plugin.run)
--- profiles/utils.py
+++ profiles/utils.py
@@ -5,12 +5,13 @@
 #
 import json
 from qgis.utils import *
-from PyQt4.QtGui import QToolBar, QDockWidget, QMessageBox
-from PyQt4.QtCore import QSettings
+from PyQt4.QtGui import QToolBar, QDockWidget, QMessageBox, QApplication, QCursor
+from PyQt4.QtCore import QSettings, QUrl, Qt
 import httplib2
 from PyQt4.Qt import QDomDocument
 from pyplugin_installer.qgsplugininstallerinstallingdialog import QgsPluginInstallerInstallingDialog
 from qgis.gui import *
+from pyplugin_installer.installer_data import repositories
 
 PLUGINS, MENUS, BUTTONS, PANELS = range(4)
 
@@ -96,7 +97,9 @@ def applyMenus(profile):
         menus.update(getMenus(None, action))
 
     for path, action in menus.iteritems():
-        if path in profile.menus or isMenuWhiteListed(path):
+        if action.isSeparator():
+            action.setVisible(True)
+        elif path in profile.menus or isMenuWhiteListed(path):
             action.setVisible(True)
             action.setText(profile.menus.get(path, action.text()))
         else:
@@ -163,12 +166,13 @@ def applyPlugins(profile):
             return False
         for p in toInstall:
             installPlugin(p)
-
+    
+    from qgis.utils import updateAvailablePlugins
     updateAvailablePlugins()
 
     settings = QSettings()
 
-    tounload = [p for p in active_plugins if p not in profile.plugins]
+    tounload = [p for p in active_plugins if p not in profile.plugins and p != "profiles"]
     for p in tounload:
         print p
         #this is a dirty trick. For some reason, calling unloadPlugin causes
@@ -178,7 +182,7 @@ def applyPlugins(profile):
         unload(p)
         settings.setValue('/PythonPlugins/' + p, False)
 
-    from qgis.utils import *
+    from qgis.utils import updateAvailablePlugins
     updateAvailablePlugins()
 
     for p in profile.plugins:
@@ -188,7 +192,6 @@ def applyPlugins(profile):
             settings.setValue('/PythonPlugins/' + p, True)
     updateAvailablePlugins()
     updatePluginManager()
-    print active_plugins
 
     return True
 
@@ -201,39 +204,95 @@ def updatePluginManager():
     installer.exportPluginsToManager()
 
 
-pluginNodes = None
+allPluginNodes = {}
 def installPlugin(pluginName):
-    global pluginNodes
-    if pluginNodes is None:
-        resp, content = httplib2.Http().request("http://plugins.qgis.org/plugins/plugins.xml?qgis=2.12")
-        if resp["status"] != "200":
-            QMessageBox.critical(iface.mainWindow(), "Plugin installation",
-                                    "Could not connect to plugin server.",
+    global allPluginNodes
+    installed = False
+    for repoName, repo in repositories.all().iteritems():        
+        if repoName not in allPluginNodes:
+            QApplication.setOverrideCursor(QCursor(QtCore.Qt.WaitCursor))
+            #TODO: use verion from current qgis, not a hardcoded one
+            resp, content = httplib2.Http().request("%s?qgis=2.12" % repo["url"])
+            if resp["status"] != "200":
+                continue
+                '''QMessageBox.critical(iface.mainWindow(), "Plugin installation",
+                                        "Could not connect to plugin server.",
+                                        QMessageBox.Ok, QMessageBox.Ok)'''
+            reposXML = QDomDocument()
+            reposXML.setContent(content.replace("& ", "&amp; "))
+            allPluginNodes[repoName] = reposXML.elementsByTagName("pyqgis_plugin")
+            QApplication.restoreOverrideCursor()
+        pluginNodes = allPluginNodes[repoName]
+        for i in range(pluginNodes.size()):
+            url = pluginNodes.item(i).firstChildElement("download_url").text().strip()
+            if ("/%s/" % pluginName) in url:
+                fileName = pluginNodes.item(i).firstChildElement("file_name").text().strip()
+                if not fileName:
+                    fileName = QFileInfo(pluginNodes.item(i).firstChildElement("download_url").text().strip().split("?")[0]).fileName()
+                name = fileName.partition(".")[0]
+                experimental = False
+                if pluginNodes.item(i).firstChildElement("experimental").text().strip().upper() in ["TRUE", "YES"]:
+                    experimental = True
+                deprecated = False
+                if pluginNodes.item(i).firstChildElement("deprecated").text().strip().upper() in ["TRUE", "YES"]:
+                    deprecated = True
+                if pluginNodes.item(i).toElement().hasAttribute("plugin_id"):
+                    plugin_id = pluginNodes.item(i).toElement().attribute("plugin_id")
+                else:
+                    plugin_id = None
+
+                plugin = {
+                    "id": name,
+                    "plugin_id": plugin_id,
+                    "name": pluginNodes.item(i).toElement().attribute("name"),
+                    "version_available": pluginNodes.item(i).toElement().attribute("version"),
+                    "description": pluginNodes.item(i).firstChildElement("description").text().strip(),
+                    "about": pluginNodes.item(i).firstChildElement("about").text().strip(),
+                    "author_name": pluginNodes.item(i).firstChildElement("author_name").text().strip(),
+                    "homepage": pluginNodes.item(i).firstChildElement("homepage").text().strip(),
+                    "download_url": pluginNodes.item(i).firstChildElement("download_url").text().strip(),
+                    "category": pluginNodes.item(i).firstChildElement("category").text().strip(),
+                    "tags": pluginNodes.item(i).firstChildElement("tags").text().strip(),
+                    "changelog": pluginNodes.item(i).firstChildElement("changelog").text().strip(),
+                    "author_email": pluginNodes.item(i).firstChildElement("author_email").text().strip(),
+                    "tracker": pluginNodes.item(i).firstChildElement("tracker").text().strip(),
+                    "code_repository": pluginNodes.item(i).firstChildElement("repository").text().strip(),
+                    "downloads": pluginNodes.item(i).firstChildElement("downloads").text().strip(),
+                    "average_vote": pluginNodes.item(i).firstChildElement("average_vote").text().strip(),
+                    "rating_votes": pluginNodes.item(i).firstChildElement("rating_votes").text().strip(),
+                    "icon": None,
+                    "experimental": experimental,
+                    "deprecated": deprecated,
+                    "filename": fileName,
+                    "installed": False,
+                    "available": True,
+                    "status": "not installed",
+                    "error": "",
+                    "error_details": "",
+                    "version_installed": "",
+                    "zip_repository": repoName,
+                    "library": "",
+                    "readonly": False
+                }
+                dlg = QgsPluginInstallerInstallingDialog(iface.mainWindow(), plugin)
+                dlg.exec_()
+                if dlg.result():
+                    QMessageBox.critical(iface.mainWindow(), "Plugin installation",
+                                    ("The %s plugin could not be installed.\n"
+                                    "The following problems were found during installation:\n%s")
+                                    % (name, dlg.result()),
                                     QMessageBox.Ok, QMessageBox.Ok)
-        reposXML = QDomDocument()
-        reposXML.setContent(content.replace("& ", "&amp; "))
-        pluginNodes = reposXML.elementsByTagName("pyqgis_plugin")
-    for i in range(pluginNodes.size()):
-        url = pluginNodes.item(i).firstChildElement("download_url").text().strip()
-        if ("/%s/" % pluginName) in url:
-            print pluginName
-            name = pluginNodes.item(i).toElement().attribute("name")
-            plugin = {"name":pluginName,
-                    "id":pluginName,
-                    "download_url":url,
-                    "filename": pluginNodes.item(i).firstChildElement("file_name").text().strip()}
-            dlg = QgsPluginInstallerInstallingDialog(iface.mainWindow(), plugin)
-            dlg.exec_()
-            if dlg.result():
-                QMessageBox.critical(iface.mainWindow(), "Plugin installation",
-                                ("The %s plugin could not be installed.\n"
-                                "The following problems were found during installation:\n%s")
-                                % (name, dlg.result()),
-                                QMessageBox.Ok, QMessageBox.Ok)
-            else:
-                loadPlugin(pluginName)
-                startPlugin(pluginName)
-                updateAvailablePlugins()
+                    return
+                else:
+                    loadPlugin(pluginName)
+                    startPlugin(pluginName)
+                    updateAvailablePlugins()
+                    return 
+
+    QMessageBox.critical(iface.mainWindow(), "Plugin installation",
+                            ("The %s plugin could not be installed.\n"
+                            "It was not found in any of the available repositories.") % pluginName,
+                            QMessageBox.Ok, QMessageBox.Ok)
 
 
 
