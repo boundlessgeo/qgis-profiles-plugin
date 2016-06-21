f968155f2df6718b7e4430de419b6dd2624aa0b2
volaya <volayaf@gmail.com>
2016-06-21 08:32:14 +0200

    fixed indentation

--- profiles/userprofiles/data_manager.py
+++ profiles/userprofiles/data_manager.py
@@ -1,6 +1,6 @@
 def apply():
 	try:
 		from processing.core.Processing import Processing
-    	Processing.activateProvider("ntv2_transformations")
-    except:
-    	pass
+		Processing.activateProvider("ntv2_transformations")
+	except:
+		pass
